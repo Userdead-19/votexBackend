a9d2413b22d4e5547416c7304d051893c350f3a6
UserDead-19 <abinav10102@gmail.com>
2024-04-02 09:38:23 +0530

    Add additional allowed headers and methods for CORS configuration

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ app.use(cors({
   origin: process.env.FRONTEND_URL || 'http://localhost:3000',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'Cookie', 'Access-Control-Allow-Origin'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Cookie', 'Access-Control-Allow-Origin', 'Access-Control-Allow-Credentials', 'Access-Control-Allow-Headers', 'Access-Control-Allow-Methods'],
 }));
 
 app.use(cookieParser());
